b7b95fcb07950263c7ff3f501db146552c483a96
Sharon Fraiman <sharon.fraiman@gm.com>
2020-07-22 11:53:20 +0300

    LET command works

--- ALPL_Interpreter.py
+++ ALPL_Interpreter.py
@@ -6,14 +6,23 @@ Code based on https://www.aosabook.org/en/500L/a-python-interpreter-written-in-p
 """
 import sys
 
+import ALPL_Lexer
+from ALPL_Lexer import RESERVED, INT , LABEL, REG, OP_ADD, OP_MULT
+
+import ALPL_Parser
+
 class Interpreter:
+    # Class constans
+    # Register file size
+    REG_FILE_SIZE = 10
+
     def __init__(self, alpl_file):
         """
         Interpreter holds the current program counter,
         the call stack return line number, and the register file
         """
         # Init object data
-        self.programCnt = 0
+        self.programLineNum = 0
         self.call_stack = []
         self.regs = [0 for i in range(self.REG_FILE_SIZE)]
         
@@ -22,11 +31,25 @@ class Interpreter:
         lines = file.readlines()
         file.close()
 
-        # Lexer
+        # Run Lexer on input lines
         self.instructions = ALPL_Lexer.createInstructions(lines)
         
+        self.endOfLineNumber = len(self.instructions)
+
     def LET_CMD (self) :
-        pass
+        """
+        Execute LET command
+        """
+        currLine = self.instructions[self.programLineNum]       
+
+        # Send to parser the line without the OpCode, which is the first token
+        parser = ALPL_Parser.parser(currLine[1:], self.regs)
+
+        # Perform the register assignment
+        self.regs = parser.assign()
+
+        # Increase program counter
+        self.programLineNum += 1
 
     def IF_CMD (self) :
         pass
@@ -43,34 +66,65 @@ class Interpreter:
     def PRINT_CMD (self) :
         pass           
         
-    def run_code(self, what_to_execute):
-        instructions = what_to_execute["instructions"]
-        for each_step in instructions:
-            instruction, argument = each_step
-            argument = self.parse_argument(instruction, argument, what_to_execute)
+    def run_code(self):
+        """
+        Main running loop of ALPL script.
+        Run each command, until reached end of line number
+        """
+        while self.programLineNum != self.endOfLineNumber :
+            currLine = self.instructions[self.programLineNum]
 
-            if instruction == "LOAD_VALUE":
-                self.LOAD_VALUE(argument)
-            elif instruction == "ADD_TWO_VALUES":
-                self.ADD_TWO_VALUES()
-            elif instruction == "PRINT_ANSWER":
-                self.PRINT_ANSWER()
-            elif instruction == "STORE_NAME":
-                self.STORE_NAME(argument)
-            elif instruction == "LOAD_NAME":
-                self.LOAD_NAME(argument)        
-        
+            firstOpCode = currLine[0]
+            opCodeType = firstOpCode[1]
+
+            # Execute command in each new line
+            if opCodeType == RESERVED :
+                opCodeName = firstOpCode[0]
+                if opCodeName == "LET" :
+                    self.LET_CMD ()
+                elif opCodeName == "IF" :
+                    self.IF_CMD ()
+                elif opCodeName == "JUMP" :
+                    self.JUMP_CMD ()
+                elif opCodeName == "CALL" :
+                    self.CALL_CMD ()
+                elif opCodeName == "RETURN" :
+                    self.RETURN_CMD ()
+                elif opCodeName == "PRINT" :
+                    self.PRINT_CMD ()
+
+            # Label adderss lines should be skipped
+            elif oCodeType == LABEL_ADDR :
+                self.programLineNum += 1
+                continue       
+    def print_inst (self) :
+        """Pretty print the instructions"""
+        print ("Parsed ALPL code :")
+        import pprint 
+        pp = pprint.PrettyPrinter(indent=4)
+        pp.pprint(self.instructions)
+
+        print ("\nRegister file :")
+        for i in range(self.REG_FILE_SIZE) :
+            print (str(i) + ") " + str(self.regs[i]))
+
+
 if __name__ == "__main__" :
-    if len(sys.argv) != 3 :
-        print ("ALPL_Interpreter syntax:\npython ALPL_Interpreter ALPL_file")
-        sys.exit(1)
-    alpl_file = sys.argv[2]
+    #if len(sys.argv) != 2 :
+    #    print ("ALPL_Interpreter syntax:\n.\\ALPL_Interpreter ALPL_file")
+    #    sys.exit(1)
+    #alpl_file = sys.argv[1]
+    
+    # Debug
+    alpl_file = r"./examples/simpleExpr.alpl"
     
     # Create Interpreter object
     interObj = Interpreter(alpl_file)
     
     # Run interpreter
     interObj.run_code()
+    interObj.print_inst()
+
     
     
     
--- ALPL_Parser.py
+++ ALPL_Parser.py
@@ -10,16 +10,24 @@ import ALPL_Lexer
 from ALPL_Lexer import RESERVED, INT , LABEL, REG, OP_ADD, OP_MULT
 
 class parser(object) :
-    # Class constans
-    # Register file size
-    REG_FILE_SIZE = 10
+    ## Class constans
+    ## Register file size
+    #REG_FILE_SIZE = 10
 
-    def __init__ (self, tokens) :
+    def __init__ (self, tokens, regs) :
         self.tokens = tokens
         self.tokenPos = 0
         
-        self.regs = [0 for i in range(self.REG_FILE_SIZE)]
+        # Register file
+        self.regs = regs
 
+    def consumeToken (self, tokenName) :
+
+        if self.tokens[self.tokenPos][0] == tokenName:
+            # Move to next token
+            self.tokenPos += 1
+        else:
+            raise Exception('Invalid Syntax')
             
     def factor(self) :
         """
@@ -46,6 +54,28 @@ class parser(object) :
         # Return Token value
         return rVal
 
+    def register(self) :
+        """
+        Grammar rule 
+        ===================
+        register : R[0-9]
+        Explanation : Register is a string that begins with 'R', and a single digit from 0 to 9
+        """
+        token = self.tokens[self.tokenPos]
+
+        if token[1] == REG :
+            regex = re.compile(r'R([0-9])')
+            match = regex.match(token[0])
+            regIdx = int(match.group(1))
+            #rVal =  self.regs[regIdx]
+        else :
+            raise Exception('Invalid Syntax')
+
+        # Move to next token
+        self.tokenPos += 1
+        # Return register index
+        return regIdx
+
     def expr(self) :
         """
         Grammar rule 
@@ -68,6 +98,27 @@ class parser(object) :
 
         # Return result value
         return result
+
+    def assign(self) :
+        """
+        Grammar rule 
+        =================== 
+        assign : R[0-9] := expr
+        """
+        # Get register index
+        regIdx = self.register()
+
+        # Consume the assignment operator
+        self.consumeToken (':=')
+
+        # Calculate assignment expression value
+        exprVal = self.expr()
+
+        # Perform the assignment
+        self.regs[regIdx] = exprVal
+
+        return self.regs
+
 
 if __name__ == "__main__" :
     exampleFile_1 = r"./examples/countTo10.alpl"
